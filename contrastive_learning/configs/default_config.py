7d9f1c71674b8ae6cfebf5ab4d3820a710eb5b91
nnminh322 <minh.nhat@mobifone.vn>
2025-08-01 14:48:55 +0700

    Update default_config.py

--- contrastive_learning/configs/default_config.py
+++ contrastive_learning/configs/default_config.py
@@ -20,8 +20,8 @@ KINETICS_STD = [0.22803, 0.22145, 0.216989]
 # --- Giai đoạn 1: Contrastive Learning ---
 STAGE1_CL_CONFIG = {
     "data_path": "/kaggle/input/rwf2000/RWF-2000",
-    "epochs": 100, # CL cần nhiều epoch hơn
-    "batch_size": 16, # Batch size lớn hơn tốt cho CL
+    "epochs": 30, # CL cần nhiều epoch hơn
+    "batch_size": 8, # Batch size lớn hơn tốt cho CL
     "learning_rate": 6e-4,
     "temperature": 0.07, # Tham số cho NT-Xent Loss
     "save_dir": f"{MODEL_SAVE_DIR}/stage_1_contrastive",
